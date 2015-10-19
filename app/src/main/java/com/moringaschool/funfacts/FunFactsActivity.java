44c2425e6899a8b7711add1db6a5b95e1ec9844b
ibra-dirir <khaliildirir@gmail.com>
2015-10-19 16:38:49 -0700

    added arrays to display more options for funfacts 10 fun facts

--- app/src/main/java/com/moringaschool/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/moringaschool/funfacts/FunFactsActivity.java
@@ -16,6 +16,7 @@ public class FunFactsActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
     // declare our view variable and assign them the views from the layout files
@@ -24,21 +25,26 @@ public class FunFactsActivity extends Activity {
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String[] facts = {
+                        "Ants stretch when they wake up in the morning.",
+                        "Ostriches can run faster than horses.",
+                        "Olympic gold medals are actually  made mostly of silver.",
+                        "You are born with 300 bones; by the time you are an adult you will have 206.",
+                        "It takes about 8 minutes for light from the sun to reach the Earth.",
+                        "Some  bamboo plants can grow almost a meter in just one day.",
+                        "The state of Florida is bigger than England.",
+                        "Some penguins can leap 2-3 meters out of the water.",
+                        "On average it takes 66 days to form a new habit.",
+                        "Mammoths still walked the earth when the Great Pyramid was being built ",
+                };
             // the button was clicked, so update  the fact label with a new fact.
                 String fact = "";
+
                 //Randomly select a fact
                 Random randomGenerator = new Random(); // Construct a new Random number genereator
-                int randomNumber = randomGenerator.nextInt(3);
-                /* convert the RandomNumber to a text fact
-                *0 = Ants stretch when they wake up in the morning.
-                *1 = Ostriches can run faster than horses.
-                *2 = Olympic gold medals are actually  made mostly of silver
-                 */
-                //if random Number equals to zero then
-                if(randomNumber==0){
-                    // set fact equals to ant fact
-                    fact = "Ants stretch when they wake up in the morning.";
-                }
+                int randomNumber = randomGenerator.nextInt(facts.length);
+               fact = facts[randomNumber];
+
                 //Update the label with our dynamic fact
                 factLabel.setText(fact);
             }
